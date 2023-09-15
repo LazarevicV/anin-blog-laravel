5b10fb1f3c66beb774d3fcfbbf8ca79fef059af1
LazarevicV <vukadinlazarevic@gmail.com>
2023-09-15 12:48:47 +0200

    minor changes

--- resources/views/posts/single-post.blade.php
+++ resources/views/posts/single-post.blade.php
@@ -13,7 +13,7 @@
     @endif
     <article class="bg-slate-800 rounded-lg shadow-md overflow-hidden">
         <div class="relative flex justify-center bg-black bg-opacity-75">
-            <img src="{{ asset('uploads/' . $blog->thumbnail) }}" class="" height="300" width="auto" alt="">
+            <img src="{{ asset('uploads/' . $blog->thumbnail) }}" class="w-500 h-500 object-cover" alt="">
             <h1
                 class="absolute text-center bottom-0 left-0 right-0 bg-black bg-opacity-75 text-white text-5xl py-2 px-4 rounded rounded-bl rounded-br">
                 {{ $blog->title }}
@@ -23,15 +23,23 @@
             <p class="text-gray-400 float-right">{{ $blog->created_at->format('F d, Y') }}</p>
         </div>
 
-        <div class="px-4 py-2 m-5 text-slate-300 text-2xl w-full text-center">
+        <div class="px-4 py-2 m-5 text-slate-300 text-2xl w-full text-left">
             {!! $blog->content !!}
         </div>
 
-        <h1 class="text-3xl m-5">Gallery</h1>
-        <div class="px-4 pb-4 flex justify-center rounded">
-            @foreach ($blog->images as $image)
-            <img src="{{ asset('uploads/' . $image->path) }}" alt="Image Description" class="rounded">
-            @endforeach
+        <h1 class="text-3xl m-5 text-slate-300">Gallery</h1>
+        <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+            <div class="-m-1 flex flex-wrap md:-m-2">
+                @foreach ($blog->images as $image)
+                <div class="flex w-1/3 flex-wrap">
+                    <div class="w-full p-1 md:p-2">
+                        <img src="{{ asset('uploads/' . $image->path) }}" alt="Image Description"
+                            class="block h-full w-full rounded-lg object-cover object-center" />
+
+                    </div>
+                </div>
+                @endforeach
+            </div>
         </div>
     </article>
 </div>
